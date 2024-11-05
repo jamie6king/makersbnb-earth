6854b90e409e4d223321f9f4883b01de6c67b409
safaa <safaa.imran7@gmail.com>
2024-11-05 16:01:50 +0000

    just deleted a test

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,15 +5,15 @@ from playwright.sync_api import Page, expect
 """
 We can render the index page
 """
-def test_get_index(page, test_web_address):
+#def test_get_index(page, test_web_address):
     # We load a virtual browser and navigate to the /index page
-    page.goto(f"http://{test_web_address}/index")
+    #page.goto(f"http://{test_web_address}/index")
 
     # We look at the <p> tag
-    p_tag = page.locator("p")
+    #p_tag = page.locator("p")
 
     # We assert that it has the text "This is the homepage."
-    expect(p_tag).to_have_text("This is the homepage.")
+    #expect(p_tag).to_have_text("This is the homepage.")
 
 def test_create_space(db_connection, page, test_web_address):
     db_connection.seed("seeds/makersbnb.sql")
