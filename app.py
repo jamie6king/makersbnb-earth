2d64be974feba9add0ac34c5dfac182c8d8fe0b9
safaa <safaa.imran7@gmail.com>
2024-11-05 15:52:21 +0000

    WIP- changed and added some routes to allow displaying of spaces from the database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, session, redirect
 from lib.database_connection import get_flask_database_connection
+from lib.space_repository import SpaceRepository
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -11,9 +12,21 @@ app = Flask(__name__)
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5001/index
+
+
 @app.route('/', methods=['GET'])
 def default_page():
-    return render_template('home.html')
+    connection = get_flask_database_connection(app)
+    repo = SpaceRepository(connection)
+    spaces = repo.all()
+    return render_template('home.html', spaces=spaces)
+
+@app.route('/home/<id>')
+def get_selected_space(id):
+    connection = get_flask_database_connection(app)
+    repo = SpaceRepository(connection)
+    space = repo.find(id)
+    return render_template("home/show-space.html", space=space)
 
 @app.route('/logged', methods=['GET'])
 def get_login_page():
@@ -30,6 +43,10 @@ def get_signup_page():
 @app.route('/signup', methods=['POST'])
 def get_sign_page():
     return render_template('signup_success.html')
+
+@app.route('/list-space', methods=['GET'])
+def get_list_space_page():
+    return render_template('list-space.html')
 
 
 # These lines start the server if you run this file directly
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,17 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(p_tag).to_have_text("This is the homepage.")
+
+def test_create_space(db_connection, page, test_web_address):
+    db_connection.seed("seeds/makersbnb.sql")
+    page.goto(f"http://{test_web_address}/list-space")
+    page.fill("input[name=spacename]", "Test Space")
+    page.fill("input[name=price]", "50")
+    page.fill("input[name=description]", "This is a test description.")
+    page.click("text='Upload Space'")
+
+def test_show_space(db_connection, page, test_web_address):
+    page.goto(f"http://{test_web_address}/home")
+    page.click("text='Cosy Apartment in the Heart of the City'")
+    h1_tag = page.selector("h1")
+    expect(h1_tag).to_heave_text("Cosy Apartment in the Heart of the City")
