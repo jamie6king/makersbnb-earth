807d9c7c73426a439cdbb66c025de0f6783243d4
clairesiqi <daysofcodingwithclaire@gmail.com>
2024-11-05 10:19:24 +0000

    Added flask route and html template for home, signup, login, account homepage

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, session, redirect
 from lib.database_connection import get_flask_database_connection
 
 # Create a new Flask app
@@ -11,12 +11,33 @@ app = Flask(__name__)
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5001/index
-@app.route('/index', methods=['GET'])
-def get_index():
-    return render_template('index.html')
+@app.route('/', methods=['GET'])
+def default_page():
+    return render_template('home.html')
+
+@app.route('/logged', methods=['GET'])
+def get_login_page():
+    return render_template('login.html')
+
+@app.route('/logged', methods=['POST'])
+def login_attempt():
+    return render_template('account_home.html')
+
+@app.route('/profile', methods=['GET'])
+def get_spaces():
+    return render_template('spaces.html')
+
+@app.route('/signup', methods=['GET'])
+def get_signup_page():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def get_sign_page():
+    return render_template('signup_success.html')
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
