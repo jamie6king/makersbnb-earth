154ed9aa5a892006d156cd15b0c93ece443edc74
Jamie King <jamiedanielking@gmail.com>
2024-11-08 09:23:53 +0000

    Added more tests

--- tests/test_booking_requests.py
+++ tests/test_booking_requests.py
@@ -1,7 +1,7 @@
-from lib.booking_requests import Booking_requests
+from lib.booking_requests import BookingRequests
 
 def test_constructs_booking_request():
-    exampledate = Booking_requests(None,1,"10/12/2024","15/12/2024",1,3,True)
+    exampledate = BookingRequests(1, "10/12/2024", "15/12/2024", 1, 3, True)
     assert exampledate.start_date == "10/12/2024"
     assert exampledate.end_date == "15/12/2024"
     assert exampledate.space_id == 1
@@ -10,5 +10,5 @@ def test_constructs_booking_request():
     assert exampledate.customer_id == 1
 
 def test_formats_booking_request():
-    exampledate = Booking_requests(1,2,"10/12/2024","15/12/2024",1,3,True)
-    assert str(exampledate) =="User ID:1,customer_id:2 desired dates:10/12/2024, to 15/12/2024, space:1, owner:3, accepted:True)"
+    exampledate = BookingRequests(1, "10/12/2024", "15/12/2024",1,3,True)
+    assert str(exampledate) =="space_id: 1, customer_id: 1, desired dates: 10/12/2024, to 15/12/2024, space: 1, owner: 3, accepted: True))"
